ebe2d85fb7a71e366c5680bae3f1a03e7b33d127
Victor de Souto <victor.souto@hotmail.com>
2023-06-30 14:22:58 -0300

    fix: port by env

--- src/index.ts
+++ src/index.ts
@@ -13,4 +13,4 @@ app.use(express.static(path.resolve(__dirname, "..", "uploads")))
 app.use(express.json())
 app.use(router)
 
-app.listen(3333, () => console.log('Server listening on port 3333'))
+app.listen(process.env.PORT, () => console.log('Server listening on port ' + process.env.PORT))
